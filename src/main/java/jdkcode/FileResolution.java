7310f87761ee44106d03f5ae7a84f71b68ffd0db
MysticalYcc <568166723@qq.com>
2020-07-26 15:03:34 +0800

    增加类加载解析阶段说明和例子

---
+++ src/main/java/jdkcode/FileResolution.java
@@ -0,0 +1,27 @@
+package jdkcode;
+
+/**
+ * @author ycc
+ * @time 14:54
+ */
+public class FileResolution {
+        interface Interface0{
+            int A = 0;
+        }
+        interface Interface1 extends Interface0{
+            int A = 1;
+        }
+        interface Interface2{
+            int A = 2;
+        }
+        static class Parents implements Interface1{
+            public static int A = 3;
+        }
+        static class Sub extends Parents implements Interface2{
+            public static  int A = 4; //此行如果注释掉则会拒绝编译
+        }
+        public static void main(String args[]){
+            System.out.println(Sub.A);
+        }
+
+}
