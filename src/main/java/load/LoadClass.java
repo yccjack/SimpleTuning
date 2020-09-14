d8109886d629688900cbf6131d27e069d75ee92f
MysticalYcc <568166723@qq.com>
2020-09-14 22:34:01 +0800

    增加读取外部class类

--- src/main/java/load/LoadClass.java
+++ src/main/java/load/LoadClass.java
@@ -16,7 +16,7 @@ public class LoadClass {
     public static void main(String[] args) throws Exception{
         FileResolution o = LoadClass.loadClass("file:///D:/code/load/", FileResolution.class);
     }
-    public static  <T>  T loadClass(String path,Class<?> clazz) throws MalformedURLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+    public static  <T>  T loadClass(String path,Class<?> clazz) throws MalformedURLException,ReflectiveOperationException {
         URL url = new URL(path);
         URLClassLoader classLoaderLeft = new URLClassLoader(new URL[]{url}, Thread.currentThread().getContextClassLoader());
         Class<?> leftClass = classLoaderLeft.loadClass(clazz.getName());
