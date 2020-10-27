3d1898638593ce81facfada62902f53f94441f06
MysticalYcc <568166723@qq.com>
2020-10-27 11:08:39 +0800

    模拟几种IO模型的调用过程

--- src/main/java/analogio/OsDis.java
+++ src/main/java/analogio/OsDis.java
@@ -1,6 +1,12 @@
 package analogio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,46 +18,75 @@ public class OsDis {
         //文件描述符
         protected SimulationFd fd;
 
-        {
-            fd = new SimulationFd();
+        SimulationFd getFd() {
+            if (fd != null) {
+                return fd;
+            }
+            fd = Kernel.getFd();;
+            return fd;
         }
-
     }
 
     //内核
-    private static class kernel {
+    private static class Kernel {
         //线程切换保存现场
         private KernelTCB tcb;
         //计算类
         private Calculation calculation;
+        //文件描述符
+        protected SimulationFd fd;
+
+        public static SimulationFd getFd() {
+            return new SimulationFd();
+        }
 
     }
 
     public static SimulationFd socket() {
 
         IOPro ioPro = new IOPro();
-        return ioPro.fd;
+        return ioPro.getFd();
     }
 
     public static SimulationFd socket(boolean non_bloking) {
 
         IOPro ioPro = new IOPro();
-        SimulationFd fd = ioPro.fd;
+        SimulationFd fd = ioPro.getFd();
         fd.bloking = non_bloking;
         return fd;
     }
 }
 
+/**
+ * 文件描述符模拟类，
+ */
 class SimulationFd {
+    private static final Logger log = LoggerFactory.getLogger(SimulationFd.class);
     //pid
     protected int numbering;
     protected boolean bloking = true;
     //每个pid的缓存数组
-    protected byte[] buffer;
+    protected volatile byte[] buffer = new byte[8 * 1024];
+    /**
+     * 非普通IO共享容器
+     */
+    protected static Queue<byte[]> buf = new ArrayBlockingQueue<>(10);
+    /**
+     * pid最小值
+     */
     static int socketIdMin = 43334;
+    /**
+     * pid最大值
+     */
     static int socketIdMax = 65535;
+    /**
+     * pid初始集合
+     */
     static ArrayBlockingQueue<Integer> usedId = new ArrayBlockingQueue<>(socketIdMax - socketIdMin + 1);
-    int read = 0;
+    /**
+     * 读取的比特数， -2为初始值，在第一次开始读取的时候变成0。
+     */
+    int read = -2;
 
     static {
         for (int i = socketIdMin; i < socketIdMax; i++) {
@@ -75,37 +110,109 @@ class SimulationFd {
     int read(byte[] buffer) {
 
         if (bloking) {
-            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-            System.out.println("陷入内核处理fd，数据未准备好，read调用阻塞，等待DMA数据加载到内核缓存！");
-            this.buffer = new byte[8 * 1024];
+            log.info("陷入内核处理fd，数据未准备好，read调用阻塞，等待DMA数据加载到内核缓存！");
             read = DMAManger.read(this.buffer);
-            System.out.println("内核缓存数据准备好，准备拷贝到用户缓存！");
+            log.info("内核缓存数据准备好，准备拷贝到用户缓存！");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.arraycopy(this.buffer, 0, buffer, 0, read);
-            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
             return read;
         }
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        if (this.buffer == null) {
-            this.buffer = new byte[8 * 1024];
-            System.out.println("陷入内核处理fd，数据未准备好，阻塞模式为Non_bloking,直接返回，请再次尝试");
-            new Thread(() -> read = DMAManger.read(buffer)).start();
+        if (this.read == -2) {
+            this.read = 0;
+            log.info("陷入内核处理fd，数据未准备好，阻塞模式为Non_bloking,直接返回，请再次尝试");
+            new Thread(() -> read = DMAManger.read(buffer), "DMA-IO").start();
         } else {
-            System.out.println("内核缓存数据准备好，阻塞模式为Non_bloking,数据已准备好，需要阻塞等待数据拷贝完成。-----准备拷贝到用户缓存！");
+            if (this.read == 0) {
+                return 0;
+            }
+            log.info("内核缓存数据准备好，阻塞模式为Non_bloking,数据已准备好，需要阻塞等待数据拷贝完成。-----准备拷贝到用户缓存！");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.arraycopy(this.buffer, 0, buffer, 0, read);
+            byte[] poll = buf.poll();
+
+            System.arraycopy(poll, 0, buffer, 0, read);
             read = -1;
-            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         }
         return read;
+    }
+
+
+    protected static Map<Integer,SimulationFd> fdMap = new ConcurrentHashMap<>();
+
+    private static volatile boolean forkIsReady = false;
+    public static boolean select() {
+        if(!forkIsReady){
+            new Thread(SimulationFd::forkThread, "select-IO").start();
+            forkIsReady = true;
+        }
+        boolean canReturn=false;
+        while (!canReturn) {
+            canReturn =  hasOneReady();
+        }
+        return true;
+    }
+    public static void eventDriven(SimulationFd fd){
+        fdMap.put(fd.numbering,fd);
+    }
+    private static boolean hasOneReady() {
+        for (SimulationFd innerFd : fdMap.values()) {
+            if (innerFd.read != -2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void forkThread() {
+        for (SimulationFd innerFd : fdMap.values()) {
+            innerFd.read = DMAManger.read(innerFd.buffer);
+        }
+    }
+
+    public static int read(int fd,byte[] buffer) {
+        log.info("内核缓存数据准备好，准备拷贝到用户缓存！");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SimulationFd simulationFd = fdMap.get(fd);
+        if(simulationFd.read==-2){
+            return 0;
+        }
+        System.arraycopy(simulationFd.buffer, 0, buffer, 0, simulationFd.read);
+        fdMap.remove(fd);
+        return simulationFd.read;
+    }
+
+    /**
+     * DMA控制器
+     */
+    static class DMAManger {
+        private static final Logger log = LoggerFactory.getLogger(DMAManger.class);
+        private static String testDate = "正在读取第一行数据，\n" +
+                "正在读取第二行数据\n";
+
+        static int read(byte[] buffer) {
+            log.info("DMA执行数据拷贝，此过程不占用CPU。");
+            byte[] bytes = testDate.getBytes();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.arraycopy(bytes, 0, buffer, 0, bytes.length);
+            buf.offer(buffer);
+            log.info("DMA拷贝数据到内核缓存成功，调用IO中断。");
+            return bytes.length;
+        }
     }
 
 }
@@ -118,25 +225,7 @@ class Calculation {
 
 }
 
-class DMAManger {
-    private static String testDate = "正在读取第一行数据，\n" +
-            "正在读取第二行数据\n";
 
-    static int read(byte[] buffer) {
-        System.out.println("--------------------------------------");
-        System.out.println("DMA执行数据拷贝，此过程不占用CPU。");
-        byte[] bytes = testDate.getBytes();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.arraycopy(bytes, 0, buffer, 0, bytes.length);
-        System.out.println("DMA拷贝数据到内核缓存成功，调用IO中断。");
-        System.out.println("--------------------------------------");
-        return bytes.length;
-    }
-}
 
 
 
--- src/main/java/analogio/UseOsDis.java
+++ src/main/java/analogio/UseOsDis.java
@@ -1,26 +1,68 @@
 package analogio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 /**
  * @author ycc
  * @time 15:17
  */
 public class UseOsDis {
-
+    private static final Logger log = LoggerFactory.getLogger(UseOsDis.class);
     public static void main(String[] args) {
-        SimulationFd simulationFd = OsDis.socket();
-        byte[] buffer_1 = new byte[8 * 1024];
-        System.out.println("调用普通IO");
-        int read = simulationFd.read(buffer_1);
-        System.out.println(new String(buffer_1));
-        System.out.println("调用NIO");
+        //testBlockIo();
+        //testNio();
+        testSelectOrPoll();
+    }
+
+    private static void testNio() {
+        log.info("调用NIO");
         SimulationFd socket = OsDis.socket(false);
+        System.out.println(socket.numbering);
         byte[] buffer_2 = new byte[8 * 1024];
         int read1 = socket.read(buffer_2);
         while (read1!=-1){
-            System.out.println("用户线程轮询检查数据是否准备好");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("用户线程轮询检查数据是否准备好");
             read1 = socket.read(buffer_2);
         }
 
-        System.out.println(new String(buffer_2));
+        log.info(new String(buffer_2));
+    }
+
+
+    public static void testBlockIo(){
+        log.info("阻塞IO调用");
+        SimulationFd simulationFd = OsDis.socket();
+        System.out.println(simulationFd.numbering);
+        byte[] buffer_1 = new byte[8 * 1024];
+        log.info("调用普通IO");
+        int read = simulationFd.read(buffer_1);
+        log.info(new String(buffer_1));
+    }
+
+    public static void testSelectOrPoll(){
+        //ET
+        log.info("select调用");
+        SimulationFd simulationFd1 = OsDis.socket();
+        SimulationFd simulationFd2 = OsDis.socket();
+
+        SimulationFd.eventDriven(simulationFd1);
+        SimulationFd.eventDriven(simulationFd2);
+        while (SimulationFd.select()){
+            for (int fdId :SimulationFd.fdMap.keySet()) {
+                byte[] buffer = new byte[8 * 1024];
+                int read = SimulationFd.read(fdId, buffer);
+                System.out.println(new String(buffer));
+            }
+        }
+
+
     }
 }
